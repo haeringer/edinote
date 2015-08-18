98be6c964bb7ecfdd86414b8e37a2d025cc4e8cb
Ben <ben@haeringer.org>
2015-08-18 18:37:50 +0200

    clean up unnecessary files

--- edinote.sql
+++ edinote.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.6.19, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: pset7
+-- Host: localhost    Database: edinote
 -- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.04.1-log
+-- Server version	5.6.19-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -104,4 +104,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-10 14:46:16
+-- Dump completed on 2015-08-18 16:32:50
--- templates/quote.php
+++
@@ -1,3 +0,0 @@
-<p>
-Current price for <?= $symbol ?> (<?= $name ?>): <?= sprintf("%0.2f", $price) ?>
-</p>
