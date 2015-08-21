fdb409a2c610b10c79a7762ceff78a4f49dead3f
Ben <ben@haeringer.org>
2015-08-21 14:33:27 +0200

    improve error checking

--- public/save.php
+++ public/save.php
@@ -9,6 +9,13 @@
     $contents = $_POST["contents"];
 
     // write contents to file
-    file_put_contents($usrdir.$filename, $contents) or die("can't open file");
+    $saved = file_put_contents($usrdir.$filename, $contents) or die("can't open file");
+
+    // error checking
+    if ($saved === false)
+    {
+        http_response_code(503);
+        exit;
+    }
 
 ?>
