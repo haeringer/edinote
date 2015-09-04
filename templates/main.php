7b3dd64d800f3e8016c1378ec62617949ec1c847
Ben <ben@haeringer.org>
2015-09-04 08:24:51 +0200

    added favicon

--- templates/main.php
+++ templates/main.php
@@ -7,7 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="">
-  <!-- <link rel="icon" href="../../favicon.ico"> -->
+  <link rel="shortcut icon" href="../img/favicon-1.ico?v=1" type="image/x-icon">
+  <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
 
   <?php if (isset($title)): ?>
       <title>Edinote: <?= htmlspecialchars($title) ?></title>
