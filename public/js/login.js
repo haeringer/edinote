8fcbbb46026b605294371e89066ffb4e12a9ad25
Ben <ben.haeringer@gmail.com>
2015-09-15 22:56:59 +0200

    login submit button state reversion

--- public/js/login.js
+++ public/js/login.js
@@ -26,14 +26,17 @@ $(function() {
 						else if (response === '2') {
 								$(".alert").css('display', 'none');
 								$(".empty-username").show();
+								submitBtnRevert();
 						}
 						else if (response === '3') {
 								$(".alert").css('display', 'none');
 								$(".empty-password").show();
+								submitBtnRevert();
 						}
 		        else if (response === '0') {
 								$(".alert").css('display', 'none');
 		            $(".invalid-login").show();
+								submitBtnRevert();
 		        }
 		        else {
 								console.log('what happened?');
@@ -50,8 +53,16 @@ $(function() {
 });
 
 function submit(e) {
-		if(e.keyCode === 13) {
+		if (e.keyCode === 13) {
 				e.preventDefault();
 				$('#submit-login').trigger('click');
 		}
+};
+
+function submitBtnRevert() {
+		$("#submit-login").delay(1000)
+		.queue(function(n) {
+				$(this).html("Log in");
+				n();
+		});
 };
